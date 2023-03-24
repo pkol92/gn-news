71c091709df3d38d89200c55f94150db531c1ade
Paulina <paulina.kolasa@espeo.eu>
2023-03-24 15:30:51 +0100

    fetch data

---
+++ src/api/axios.ts
@@ -0,0 +1,15 @@
+import axiosLib from 'axios';
+
+const BASE_URL = 'https://newsapi.org/v2/';
+const API_KEY = 'e412a86a7f0c4c779917cd632550bf20';
+
+export enum ApiUrl {
+  country = 'top-headlines?country=',
+}
+
+export const apiSecure = axiosLib.create({
+  baseURL: BASE_URL,
+  headers: {
+    Authorization: API_KEY,
+  },
+});
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import { useGetNewsFromCountry } from '../../api/getNews';
 import { DropdownElement } from './Dropdown/Dropdown';
 
 import { HeaderWrapper, LogoWrapper } from './Header.styles';
 import { Modal } from './Modal/Modal';
 
 export const Header = () => {
+  const { data } = useGetNewsFromCountry({ country: 'us' });
+  console.log(data);
   return (
     <HeaderWrapper>
       <LogoWrapper> gnNews</LogoWrapper>
