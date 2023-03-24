1b70f8f5b14885174a315cf0bc387bf35f61f77b
Paulina <paulina.kolasa@espeo.eu>
2023-03-24 15:41:02 +0100

    add panel

--- src/components/Panel/Panel.tsx
+++ src/components/Panel/Panel.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useGetNewsFromCountry } from '../../api/getNews';
 
 export const Panel = () => {
-  const { data } = useGetNewsFromCountry({ country: 'us' });
-  console.log(data.articles);
+  const { data } = useGetNewsFromCountry({ country: 'pt' });
+  console.log(data);
   return <div>Panel</div>;
 };
