1a93edcef4b9f730762513f10c691569c8ae67fb
Paulina <paulina.kolasa@espeo.eu>
2023-03-31 16:52:59 +0200

    add  news to home panel

--- src/components/MainPanel/MainPanel.tsx
+++ src/components/MainPanel/MainPanel.tsx
@@ -1,18 +1,24 @@
 import React from 'react';
+import { useQuery } from 'react-query';
 import { useOutlet } from 'react-router-dom';
+import { countryQuery } from '../../api/getNews';
+import { Footer } from '../Footer/Footer';
+import { Panel } from '../Panel/Panel';
 import { SideMenu } from '../SideMenu/SideMenu';
-import { InfoWrapper, MainWrapper } from './MainPanel.styles';
+import { MainWrapper } from './MainPanel.styles';
 
 export const MainPanel = () => {
   const outlet = useOutlet();
+  const { data, isLoading, isError } = useQuery(countryQuery('pl'));
+
   return (
     <MainWrapper>
       <SideMenu />
       {outlet || (
-        <InfoWrapper>
-          <h3>Witamy w gnNews!</h3>
-          <h6>Wybierz kraj, z którego chcesz zobaczyć wiadomości.</h6>
-        </InfoWrapper>
+        <>
+          <Panel data={data} isError={isError} isLoading={isLoading} />
+          <Footer articlesNumber={data?.articles.length} />
+        </>
       )}
     </MainWrapper>
   );
